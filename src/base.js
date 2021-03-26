b59ae022c79a38f874b644154a027dd52d0bf108
Nrazsfi <59966837+Nrazsfi@users.noreply.github.com>
2021-03-26 19:32:48 +0100

    adding firebase

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDXUCNSpi5u07F76httlCTXAA4mPVQlEHs",
-  authDomain: "catch-of-the-day-wes-bos-2.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-wes-bos-2.firebaseio.com"
+  apiKey: "AIzaSyBOJlN4YZYQkUBP0ySIOrScc0J0UAI8V2U",
+    authDomain: "cotd-fromlesson23.firebaseapp.com",
+    databaseURL: "https://cotd-fromlesson23-default-rtdb.firebaseio.com"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
